39d7b34161ae697dcdd7fbe8923f54f75e77b38b
Enrika <23ecardeno@wakatipu.school.nz>
2026-03-19 11:33:19 +1300

    basic

---
+++ README.py
@@ -0,0 +1,11 @@
+# PROGRESS-CHECK
+student = {
+    "first_name": "Jack",
+    "surname": "Frost",
+    "year": "12",
+    "level": "Marit",
+    "subjects": ["Music", "PE"]
+}
+
+print("These are Jacck's subjects", student["subjects"])
+
